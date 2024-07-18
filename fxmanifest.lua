6fc9a7226039e708fa6892e6d1337a08eafe61aa
cirne <135841248+cirnee@users.noreply.github.com>
2024-07-18 14:13:43 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'cirne'
+description 'UrbanCX quitar enderezar coches'
+version '1.0.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'config/config.lua'
+}
